f7a21fb901f7ef1ee3016648389fac383e61b0e7
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-16 01:01:32 -0700

    finally this type worked, I am going to explain in the comments how.

--- src/SumTarget.java
+++ src/SumTarget.java
@@ -2,20 +2,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SumTarget {
-    public static int [] sumTarget(int [] nums, int target){
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            int complement = target- nums[i];
-            if(map.containsKey(complement)){
-                return new int[]{map.get(complement), i};
+
+        public static int[] sumTarget(int nums[], int target) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                int complement = target - nums[i];
+                if (map.containsKey(complement)) {
+                    return new int[]{map.get(complement), i};
+                }
+                map.put(nums[i], i);
             }
-            map.put(nums[i],i );
-        }throw new IllegalArgumentException("No two sum solution");
+            return null;
 
-    }
-    public static void main(String[] args) {
+        }
+        public static void main(String[] args) {
         int [] nums = {2,7,11,15};
-        sumTarget(nums, 9);
+        sumTarget(nums,9);
+
 
     }
+
 }
